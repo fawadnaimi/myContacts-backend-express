38e0e11fcfa9a148f782098088c8321e20716272
Fawad Naimi <fnaimi@outlook.com>
2023-04-29 11:29:57 +0200

    Controller, middleware, and error handling were added. Connection with MongoDB is also successfully done

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,23 +1,25 @@
-const getContacts = (req, res) => {
+const asyncHandler = require("express-async-handler");
+
+const getContacts = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get all contacts" });
-};
-const addContact = (req, res) => {
+});
+const addContact = asyncHandler(async (req, res) => {
   const { name, email, phone } = req.body;
   if (!name || !email || !phone) {
     res.status(400);
     throw new Error("All fields are required !");
   }
   res.status(200).json({ message: "Get all contacts" });
-};
-const getContact = (req, res) => {
+});
+const getContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Get individual contact ${req.params.id}` });
-};
-const updateContact = (req, res) => {
+});
+const updateContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update contact for ${req.params.id} ` });
-};
-const deleteContact = (req, res) => {
+});
+const deleteContact = asyncHandler(async (req, res) => {
   res.json({ message: `Delete contact for ${req.params.id} ` });
-};
+});
 
 module.exports = {
   getContacts,
